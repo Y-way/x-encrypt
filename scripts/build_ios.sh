c733c26d93128995421b60363cfc6757dd1a9bb7
Y-way <young_way2007@126.com>
2025-02-25 11:12:12 +0800

    iOS And Linux build script

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -4,6 +4,6 @@ cd ../XEncrypt/
 cmake -B build_ios -GXcode -DCMAKE_SYSTEM_NAME=iOS "-DCMAKE_OSX_ARCHITECTURES=arm64"
 cmake --build build_ios --config Release
 mkdir -p plugin_ios/Plugins/iOS/
-cp build_ios/Release/*.a plugin_ios/Plugins/iOS/*.a 
+cp build_ios/Release/*.a plugin_ios/Plugins/iOS/
 
 popd
--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -5,8 +5,12 @@ cd ../XEncrypt/
 cmake -B build_linux64 -DBUILD_SHARED_LIBS=ON
 cmake --build build_linux64 --config Release
 
+cmake -B build_linux64_static -DBUILD_SHARED_LIBS=OFF
+cmake --build build_linux64_static --config Release
+
 mkdir -p plugin_linux/Plugins/x86_64
+
 cp build_linux64/*.so plugin_linux/Plugins/x86_64/
-cp build_linux64/*.a plugin_linux/Plugins/x86_64/
+cp build_linux64_static/*.a plugin_linux/Plugins/x86_64/
 
 popd
