169ff9404216cb730056cc5bb167c71cf680ef36
Mac Dev <macdev@MacdeMac-mini.local>
2025-02-27 18:55:25 +0800

    make macOS shared library x86_64

--- scripts/build_osx.sh
+++ scripts/build_osx.sh
@@ -3,11 +3,11 @@ pushd .
 cd ../XEncrypt/
 
 mkdir -p build_osx && cd build_osx
-cmake -GXcode ../ -DBUILD_SHARED_LIBS=ON
+cmake -GXcode ../ -DBUILD_SHARED_LIBS=ON -DARCHS="x86_64"
 cd ..
 cmake --build build_osx --config Release
-mkdir -p plugin_osx/Plugins/XEncrypt.bundle/Contents/MacOS/
+mkdir -p plugin_osx/Plugins/
 
-cp build_osx/Release/XEncrypt.bundle/Contents/MacOS/XEncrypt plugin_osx/Plugins/XEncrypt.bundle/Contents/MacOS/XEncrypt
+cp build_osx/Release/* plugin_osx/Plugins/
 
 popd
