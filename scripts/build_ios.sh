ed3d40d02b5c2b70d74a2a4747af8fe68656a97c
Y-way <young_way2007@126.com>
2025-02-27 10:24:36 +0800

    Support tvOS

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -1,7 +1,7 @@
 pushd .
 
 cd ../XEncrypt/
-cmake -B build_ios -GXcode -DCMAKE_SYSTEM_NAME=iOS "-DCMAKE_OSX_ARCHITECTURES=arm64"
+cmake -B build_ios -GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_ARCHITECTURES=arm64 -DPLAT=iOS
 cmake --build build_ios --config Release
 mkdir -p plugin_ios/Plugins/iOS/
 cp build_ios/Release/*.a plugin_ios/Plugins/iOS/
