4c1b4f8c130c64a32fb2dea5852edd26f58ceead
Y-way <young_way2007@126.com>
2025-03-20 15:06:38 +0800

    Update XResult.h

--- XEncrypt/encrypt/service/XResult.h
+++ XEncrypt/encrypt/service/XResult.h
@@ -6,6 +6,7 @@
 
 namespace xencrypt
 {
+    /// @brief 加密/解密结果
     class XENCRYPT_API XResult final
     {
     public:
@@ -15,17 +16,18 @@ namespace xencrypt
         XResult(const XResult&) = delete;
         XResult(XResult&&) = delete;
         XResult& operator=(const XResult&) = delete;
-        
+        /// @brief 获取结果数据缓存地址指针.该数据在XResult对象销毁时会被释放.
+        /// @return 结果数据缓存地址
         byte* GetData() const
         {
             return _context != nullptr ? _context->GetResultData() : nullptr;
         }
-
+        /// @brief 获取结果数据大小
         int64_t GetDataSize() const
         {
             return _context != nullptr ? _context->GetResultDataLength() : 0;
         }
-
+        /// @brief 加密/解密结果状态
         ResultCode GetCode() const { return _resultCode; }
     private:
 
