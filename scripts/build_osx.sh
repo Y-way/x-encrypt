abc20e5fe592b3cc840883c2050a7b8a48e03e85
Y-way <young_way2007@126.com>
2025-03-24 13:54:02 +0800

    支持生成静态库

--- scripts/build_osx.sh
+++ scripts/build_osx.sh
@@ -11,7 +11,7 @@ mkdir -p plugin_osx/Plugins/
 cp build_osx/Release/* plugin_osx/Plugins/
 
 mkdir -p build_osx_a && cd build_osx_a
-cmake -GXcode ../ -DBUILD_SHARED_LIBS=FF -DARCHS="x86_64"
+cmake -GXcode ../ -DBUILD_SHARED_LIBS=OFF -DARCHS="x86_64"
 cd ..
 cmake --build build_osx_a --config Release
 
