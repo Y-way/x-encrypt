a31c08e29c96ea867427fd0d89df0e13bbdbd96c
Y-way <young_way2007@126.com>
2025-03-24 13:55:28 +0800

    支持x86_64和arm64

--- scripts/build_osx.sh
+++ scripts/build_osx.sh
@@ -1,20 +1,31 @@
+echo 0
+pushd .
+
+cd ../XEncrypt/
+mkdir -p plugin_osx/Plugins/
+
+# Building for shared library. Supporting x86_64 and arm64 architectures.
+mkdir -p build_osx && cd build_osx
+cmake -GXcode ../ -DBUILD_SHARED_LIBS=ON -DARCHS="\$(ARCHS_STANDARD)"
+
+cd ..
+cmake --build build_osx --config Release
+
+cp ./build_osx/Release/* ./plugin_osx/Plugins/
+
+popd
+
+# Building for static library. Supporting x86_64 and arm64 architectures.
 pushd .
 
 cd ../XEncrypt/
 
-mkdir -p build_osx && cd build_osx
-cmake -GXcode ../ -DBUILD_SHARED_LIBS=ON -DARCHS="x86_64"
-cd ..
-cmake --build build_osx --config Release
-mkdir -p plugin_osx/Plugins/
-
-cp build_osx/Release/* plugin_osx/Plugins/
-
 mkdir -p build_osx_a && cd build_osx_a
-cmake -GXcode ../ -DBUILD_SHARED_LIBS=OFF -DARCHS="x86_64"
+cmake -GXcode ../ -DBUILD_SHARED_LIBS=OFF -DARCHS="\$(ARCHS_STANDARD)"
+
 cd ..
 cmake --build build_osx_a --config Release
 
-cp build_osx_a/Release/* plugin_osx/Plugins/
+cp ./build_osx_a/Release/* plugin_osx/Plugins/
 
 popd
